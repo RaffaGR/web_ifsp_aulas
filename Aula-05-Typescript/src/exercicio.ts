95f880e6bda975394706594f63f4a159eaf778a4
RafaGR <110624346+RaffaGR@users.noreply.github.com>
2026-03-05 20:39:50 -0300

    ex 1 ao 4

--- Aula-05-Typescript/src/exercicio.ts
+++ Aula-05-Typescript/src/exercicio.ts
@@ -1,9 +1,56 @@
+// 1
 // let maiuscula: string = "TexT";
 
-function imprimeMaiusculas(maiuscula:string = "Text") {
+function imprimeMaiusculas(maiuscula: string = "Text") {
     return console.log(maiuscula.toUpperCase());
 }
 
-imprimeMaiusculas;
+// imprimeMaiusculas();
 
-// function imprimeMaiusculasArrayFunction(maiuscula:string = "Text"): string => console.log(maiuscula.toUpperCase());
+const imprimeMaiusculasArrayFunction = (maiuscula: string = "Text"): void => console.log(maiuscula.toUpperCase());
+
+// imprimeMaiusculasArrayFunction();
+
+// 2
+function elevar(n1: number, n2: number) {
+    return n1 ** n2;
+}
+
+// 3
+function dobrarNumeros(aN: number[]): number[] {
+    return aN.map(numero => numero * 2);
+}
+
+/* function dobrarNumerosOPT2(aN: number[]): number[] {
+    let nA: number[] = [];
+    for (let i = 0; i < aN.length; i++) {
+        nA.push(aN[i] * 2);
+    }
+    return nA;
+} */
+
+// const original = [1, 2, 3, 4, 5];
+// const dobrados = dobrarNumeros(original);
+// console.log(dobrados);
+
+// 4
+type Funcionario = {
+    nome: string;
+    cargo: string;
+};
+
+const funcionarios: Funcionario[] = [
+    { nome: " Marcos ", cargo: " Desenvolvedor " },
+    { nome: " Fernanda ", cargo: " Gerente " },
+    { nome: " Carlos ", cargo: " Desenvolvedor " },
+    { nome: " Joana ", cargo: " Analista " }
+];
+const cargoDesejado: string = " Desenvolvedor ";
+
+function filtroCargo(funs: Funcionario[]): Funcionario[] {
+    return funs.filter( fun => fun.cargo == " Desenvolvedor ");
+}
+
+const cargosFiltrados = filtroCargo(funcionarios);
+
+// 5 
